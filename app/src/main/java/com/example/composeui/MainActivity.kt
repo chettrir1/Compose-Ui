f9b22f3d6c1fb2e93059fc4466a818fd0ae47971
Raju <csta.puja@gmmail.com>
2024-01-15 00:51:45 -0500

    custom image card added with compose view

--- app/src/main/java/com/example/composeui/MainActivity.kt
+++ app/src/main/java/com/example/composeui/MainActivity.kt
@@ -1,51 +1,89 @@
 package com.example.composeui
 
 import android.os.Bundle
+import android.provider.CalendarContract
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(color = Color.Green),
-                verticalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Row(
+            val painter = painterResource(id = R.drawable.olaf)
+            val description = "Karmit in the snow"
+            val title = "Karmit is playing in the snow"
+            ImageCard(
+                painter = painter,
+                contentDescription = description,
+                title = title
+            )
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = Modifier.padding(16.dp)) {
+        Card(
+            modifier = modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp)
+        ) {
+            Box(modifier = Modifier.height(200.dp)) {
+                Image(
+                    modifier = Modifier.fillMaxSize(),
+                    painter = painter,
+                    contentDescription = contentDescription,
+                    contentScale = ContentScale.Crop
+                )
+                Box(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .background(color = Color.Red),
-                    horizontalArrangement = Arrangement.SpaceAround,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = "Hello", color = Color.White)
-                    Text(text = "World!", color = Color.White)
-                }
-                Column(
+                        .fillMaxSize()
+                        .background(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(
+                                    Color.Transparent,
+                                    Color.Black
+                                ),
+                                startY = 400f,
+                            )
+                        )
+                )
+                Box(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .background(color = Color.Red),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.SpaceAround
+                        .fillMaxSize()
+                        .padding(12.dp),
+                    contentAlignment = Alignment.BottomStart
                 ) {
-                    Text(text = "Hello", color = Color.White)
-                    Text(text = "World!", color = Color.White)
+                    Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
                 }
             }
-
         }
     }
 }

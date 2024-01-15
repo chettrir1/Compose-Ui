a94eae9b775ea61a016d82b9b4cce550dcf8865b
Raju <csta.puja@gmmail.com>
2024-01-15 01:11:40 -0500

    custom text style added with text modification

--- app/src/main/java/com/example/composeui/MainActivity.kt
+++ app/src/main/java/com/example/composeui/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.composeui
 
 import android.os.Bundle
-import android.provider.CalendarContract
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -11,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -23,7 +21,16 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -35,25 +42,32 @@ class MainActivity : ComponentActivity() {
             val description = "Karmit in the snow"
             val title = "Karmit is playing in the snow"
             ImageCard(
-                painter = painter,
-                contentDescription = description,
-                title = title
+                painter = painter, contentDescription = description, title = title
             )
+
+            val fontFamily = FontFamily(
+                Font(R.font.lexend_thin, FontWeight.Thin),
+                Font(R.font.lexend_semi_bold, FontWeight.SemiBold),
+                Font(R.font.lexend_regular, FontWeight.Normal),
+                Font(R.font.lexend_medium, FontWeight.Medium),
+                Font(R.font.lexend_light, FontWeight.Light),
+                Font(R.font.lexend_extra_light, FontWeight.ExtraLight),
+                Font(R.font.lexend_extra_bold, FontWeight.ExtraBold),
+                Font(R.font.lexend_bold, FontWeight.Bold),
+                Font(R.font.lexend_black, FontWeight.Black)
+            )
+            CustomText(title = "Jetpack Compose", fontFamily = fontFamily)
         }
     }
 }
 
 @Composable
 fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
+    painter: Painter, contentDescription: String, title: String, modifier: Modifier = Modifier
 ) {
     Box(modifier = Modifier.padding(16.dp)) {
         Card(
-            modifier = modifier.fillMaxWidth(),
-            shape = RoundedCornerShape(16.dp)
+            modifier = modifier.fillMaxWidth(), shape = RoundedCornerShape(16.dp)
         ) {
             Box(modifier = Modifier.height(200.dp)) {
                 Image(
@@ -68,8 +82,7 @@ fun ImageCard(
                         .background(
                             brush = Brush.verticalGradient(
                                 colors = listOf(
-                                    Color.Transparent,
-                                    Color.Black
+                                    Color.Transparent, Color.Black
                                 ),
                                 startY = 400f,
                             )
@@ -86,4 +99,51 @@ fun ImageCard(
             }
         }
     }
+}
+
+@Composable
+fun CustomText(
+    modifier: Modifier = Modifier,
+    title: String,
+    fontFamily: FontFamily
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color(0xFF101010))
+            .padding(16.dp),
+    ) {
+        Text(
+            text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Green,
+                        fontSize = 50.sp
+                    )
+                ) {
+                    append("J")
+                }
+                append("etpack ")
+                withStyle(
+                    style = SpanStyle(
+                        color = Color.Green,
+                        fontSize = 50.sp
+                    )
+                ) {
+                    append("C")
+                }
+                append("ompose ")
+            }, style = TextStyle(
+                color = Color.White,
+                fontFamily = fontFamily,
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp,
+                fontStyle = FontStyle.Italic,
+                textAlign = TextAlign.Center,
+                textDecoration = TextDecoration.Underline
+            )
+        )
+
+    }
+
 }

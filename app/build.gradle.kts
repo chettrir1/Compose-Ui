238a1f8fb8ea75b98855a7a0fe6012b8a5405200
Raju <csta.puja@gmmail.com>
2024-01-15 17:12:16 -0500

    greeting section and chip section composable added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material:1.6.0-rc01")
+
     implementation("androidx.constraintlayout:constraintlayout-compose-android:1.1.0-alpha13")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")

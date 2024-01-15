7d7cd407b5658f28d8f0dad3d9ccca31fbf39815
Raju <csta.puja@gmmail.com>
2024-01-14 21:39:39 -0500

    column, row, modifiers and size prictice

--- app/src/main/java/com/example/composeui/MainActivity.kt
+++ app/src/main/java/com/example/composeui/MainActivity.kt
@@ -3,11 +3,48 @@ package com.example.composeui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = Color.Green),
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(color = Color.Red),
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = "Hello", color = Color.White)
+                    Text(text = "World!", color = Color.White)
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(color = Color.Red),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceAround
+                ) {
+                    Text(text = "Hello", color = Color.White)
+                    Text(text = "World!", color = Color.White)
+                }
+            }
 
         }
     }
